6a3f34f423f10f37db649ab75dcdf18009021f6c
LuispeFigueroa <fig24087@uvg.edu.gt>
2026-04-24 00:42:49 -0600

    preprar main.py para los routers

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers import productos, clientes, ventas, reportes
 
-app = FastAPI()
+app = FastAPI(title="Tienda LP")
 
 app.add_middleware(
     CORSMiddleware,
@@ -10,6 +11,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(productos.router, prefix="/productos",tags=["Productos"])
+app.include_router(clientes.router, prefix="/clientes",tags=["Clientes"])
+app.include_router(ventas.router, prefix="/ventas",tags=["Ventas"])
+app.include_router(reportes.router, prefix="/reportes",tags=["Reportes"])
+
 @app.get("/")
 def root():
-    return {"message": "API funcionando"}
+    return {"message": "Tienda LP en linea"}
